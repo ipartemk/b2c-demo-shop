e4c0ed22f16c367009fc36c8d560002c27808d54
Artem Kuznetsov <artem.kuznetsov@spryker.com>
2024-02-26 13:18:28 +0100

    task-management. DB schema

---
+++ src/Orm/Zed/Task/Persistence/MeuTaskTagRelationQuery.php
@@ -0,0 +1,23 @@
+<?php
+
+/**
+ * This file is part of the Spryker Commerce OS.
+ * For full license information, please view the LICENSE file that was distributed with this source code.
+ */
+
+namespace Orm\Zed\Task\Persistence;
+
+use Orm\Zed\Task\Persistence\Base\MeuTaskTagRelationQuery as BaseMeuTaskTagRelationQuery;
+
+/**
+ * Skeleton subclass for performing query and update operations on the 'meu_task_tag_relation' table.
+ *
+ *
+ *
+ * You should add additional methods to this class to meet the
+ * application requirements. This class will only be generated as
+ * long as it does not already exist in the output directory.
+ */
+class MeuTaskTagRelationQuery extends BaseMeuTaskTagRelationQuery
+{
+}
